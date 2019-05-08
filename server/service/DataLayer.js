d8a6f0a41f1c856d06b31038124a32f8bb771750
Davide Calabrò <davide.calabro@mail.polimi.it>
2019-05-08 10:23:14 +0200

    Add data layer and knex dependency

---
+++ server/service/DataLayer.js
@@ -0,0 +1,15 @@
+const sqlDbFactory = require("knex");
+
+let sqlDb = sqlDbFactory({
+  client: "pg",
+  connection: process.env.DATABASE_URL,
+  ssl: true,
+  debug: true
+});
+
+function setupDataLayer() {
+  console.log("Setting up data layer");
+  return booksDbSetup(sqlDb);
+}
+
+module.exports = { database: sqlDb, setupDataLayer };
