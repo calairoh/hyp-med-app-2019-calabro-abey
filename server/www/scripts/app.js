34b107a8dce383c3dd2ce72105984237b6afb2b1
Davide Calabrò <davide.calabro@mail.polimi.it>
2019-05-22 14:39:37 +0200

    End backend login and register

--- server/www/scripts/app.js
+++ server/www/scripts/app.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
     signIn();
+    signUp();
     getBooks();
 });
 
@@ -25,13 +26,47 @@ function signIn(){
                 password: password
             },
             success: function(result){
-                if(result === true)
-                    alert("login OK");
+                if(result.code === 200)
+                    location.href = "/index.html"
                 else
                     alert("login failure");
             }
         });
-    })
+    });
+}
+
+function signUp(){
+    $('.form-signup').on('submit', function(e){
+        e.preventDefault();
+        var url = $(this).data('action');
+        var formSerialize = $(this).serializeArray();
+
+        $.ajax({
+            url: url,
+            method: 'POST',
+            dataType: 'json',
+            data: {                
+                username: formSerialize[0].value,
+                email: formSerialize[1].value,
+                password: formSerialize[2].value,
+                firstName: formSerialize[4].value,
+                lastName: formSerialize[5].value,
+                address: formSerialize[6].value,
+                city: formSerialize[7].value,
+                postalCode: formSerialize[8].value,
+                country: formSerialize[9].value
+            },
+            success: function(result){
+                if(result.code === 200){
+                    alert("Registrazione OK");
+                    location.href = "/account/login.html";
+                }
+                else {
+                    alert(result.msg);
+                }
+            }
+        });
+    });
 }
 
 function validator(){
