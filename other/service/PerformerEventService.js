9ad3802e2c22a349f3735e43edcb6d04bdac83cb
Davide Calabrò <davide.calabro@mail.polimi.it>
2019-08-24 19:51:20 +0200

    Fix publish db problem

--- other/service/PerformerEventService.js
+++ other/service/PerformerEventService.js
@@ -12,8 +12,8 @@ exports.performerEventDbSetup = function(database) {
       return database.schema.createTable("performerEvent", table => {
         table.integer('eventId');
         table.integer("performerId");
-        table.foreign("eventId").references("event.id");
-        table.foreign("performerId").references("performer.id");
+        //table.foreign("eventId").references("event.id");
+        //table.foreign("performerId").references("performer.id");
       });
     }
     
