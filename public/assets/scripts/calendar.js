97586b5a0a530c39b65874946953dce6cea09d12
Davide Calabrò <davide.calabro@mail.polimi.it>
2019-09-02 14:21:07 +0200

    Fix bissextile checker

--- public/assets/scripts/calendar.js
+++ public/assets/scripts/calendar.js
@@ -129,11 +129,11 @@ function getDaysFromMonth(m, y){
 }
 
 function checkBissextile(y){
-    if((y % 4) !== 0)
-        return false;
-    if((y % 100) === 0)
+    if((y % 4) === 0)
         return true;
     if((y % 400) === 0)
         return true;
+    if((y % 100) === 0)
+        return false;
     return false;
 }
